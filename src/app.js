3042ac8084f6cf37f6ff593d1b202044d9d9b695
larytet <larytet@yahoo.com>
2020-08-02 01:33:38 +0300

    loop forever

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const { chromium } = require('playwright-chromium');
         const page = await browser.newPage();
         await page.goto('http://whatsmyuseragent.org/');
         await page.screenshot({ path: `example.png` });
-
         await browser.close();  
     }
 })();
