185286db642b6c6dd5a265a3695fad5191a552cc
larytet <larytet@yahoo.com>
2020-08-01 23:23:22 +0300

    VNC

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ const { chromium } = require('playwright-chromium');
   const page = await browser.newPage();
   await page.goto('http://whatsmyuseragent.org/');
   await page.screenshot({ path: `example.png` });
-  await browser.close();  await browser.close();
+  await browser.close();  
 })();
