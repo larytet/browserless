1bf0335b7f6d5451fd4c35f1fab1eb12e1d6116c
larytet <larytet@yahoo.com>
2020-08-02 20:20:48 +0300

    readme

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ const { chromium } = require('playwright-chromium');
 (async () => {
     while (true) {
         const browser = await chromium.launch({ headless: false, slowMo: 50 });
-        const page = await browser.newPage();
+        const context = await browser.newContext();
+        const page = await context.newPage();
         await page.goto('http://whatsmyuseragent.org/');
         await page.screenshot({ path: `example.png` });
         await browser.close();  
