ca23d2e71c3534f34e819a0037439423ffa91521
larytet <larytet@yahoo.com>
2020-08-03 23:37:39 +0300

    readme

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 const { chromium } = require('playwright-chromium');
 
 (async () => {
-    while (true) {
-        const browser = await chromium.launch({ headless: false, slowMo: 50 });
+        // https://playwright.dev/#version=v1.2.1&path=docs%2Fapi.md&q=browsertypelaunchoptions
+        const browser = await chromium.launch({ headless: false, slowMo: 0 });
         const context = await browser.newContext();
-        const page = await context.newPage();
-        await page.goto('http://whatsmyuseragent.org/');
-        await page.screenshot({ path: `example.png` });
+        while (true) {
+            const page = await context.newPage();
+            await page.goto('http://whatsmyuseragent.org/');
+            await page.screenshot({ path: `example.png` });
+            page.close()
+        }
         await browser.close();  
-    }
 })();
